41b5e9ae5afc7aaebcc3e1b1a5f6b10c8bfacd95
aniket-kapoor <aniketkapoor.cse@gmail.com>
2026-01-03 15:54:38 +0530

    chore: save local changes before rebase

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from AuthenticationApp.dependencies.database import Base
+from ..App.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
